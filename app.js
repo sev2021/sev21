da6423313723b8b59afd38648b91684cc836fe42
codeacade <codeacade@gmail.com>
2022-08-09 21:50:52 +0100

    IPs listed

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
 const express = require('express');
 const app = express();
-let i = 0;
-let timeMin = 0;
+const fs = require('fs');
+var i = 2;
+var timeMin = 0;
+
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
-	res.send(
-	`<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Heroku tricks</title>
-	</head>
-	<body>
-		<p>This page was open ${ i } times.</p>
-		<p>Up-time is ${ timeMin } minutes.</p>
-		<a href="reset"> RESET UP-TIME </a>
-	  </body>
-	</html>`
-	);
-	i++;
-});
-
-app.get("/reset", (req, res) => {
-	res.send(
-	`<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Heroku tricks</title>
-	</head>
-	<body>
-		<p>Up-Time got reset after ${ timeMin } minutes.</p>
-		<a href="/"> RETURN </a>
-	  </body>
-	</html>`
-	);
-	timeMin = 0;
+	
+	fs.readFile('./mydata.txt', (err, data) => {
+		if(err) var dataString = err;
+		else var dataString = data.toString();
+		dataString += `${ i++ / 2 } - ${ req.ip }<br/>`;
+		dataString += '\n';
+		fs.writeFile('./mydata.txt', dataString, ()=>{});
+	
+		res.send(
+		`<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<title>Heroku tricks</title>
+			<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+			<link rel="stylesheet" href="./style.css">
+		</head>
+		<body>
+			<div class="container mt-5 bg-info">
+				<h3>This page was open ${ i++ } times.</hh3>
+				<h4>Up-time is ${ timeMin } minutes.</h4>
+				<p>String data:<br/> ${ dataString }</p>
+				<a href="reset"> RESET UP-TIME </a>
+			</div>
+		  </body>
+		</html>`
+		);
+	});
 });
 
 app.listen(process.env.PORT || 3000);
 
-// setTimeout(() => {i += 100;}, 10000);
-setInterval(() => {timeMin ++;}, 60000);
 
--- spp.js
+++
@@ -1,4 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {res.end("jjj")});
-
-server.listen(3000);
